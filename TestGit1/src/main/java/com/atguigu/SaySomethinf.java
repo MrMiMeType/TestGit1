a8eed7073fd3199522aeab7b4085c886a2d3bf39
MrMiMeType <zhangjianchun8023@163.com>
2018-06-29 23:15:09 +0800

    来自mime的修改

--- TestGit1/src/main/java/com/atguigu/SaySomethinf.java
+++ TestGit1/src/main/java/com/atguigu/SaySomethinf.java
@@ -4,6 +4,19 @@ public class SaySomethinf {
 
 	public static void main(String[] args) {
 		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
+		System.out.println("what shall i say!");
 	}
 
 }
